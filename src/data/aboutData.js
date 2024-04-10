34f2007a1692e51878fe0f26ad36e890118cf38c
satheeshraj05 <satheeshraj333@gmail.com>
2024-04-10 21:40:51 +0530

    Initial commit

---
+++ src/data/aboutData.js
@@ -0,0 +1,8 @@
+import { pinkThemeboy } from "../theme/images"
+
+export const aboutData = {
+    title: "Who I am",
+    description1: "My name's fjxf I'm a Web Designer and developer based in Coimbatore, India.",
+    description2: "I love to learn and explore new arenas. ",
+    image: pinkThemeboy
+}
