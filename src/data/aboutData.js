9f007978b9d33254d132e7d6de82f304816cda00
Satheesh <57912584+Satheeshraj05@users.noreply.github.com>
2024-09-12 21:36:14 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,7 +2,7 @@ import { pinkThemeboy } from "../theme/images"
 
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's fjxf I'm a Web Designer and developer based in Coimbatore, India.",
+    description1: "My name's Satheesh R. I'm a Web Designer and developer based in Coimbatore, India.",
     description2: "I love to learn and explore new arenas. ",
     image: pinkThemeboy
 }
